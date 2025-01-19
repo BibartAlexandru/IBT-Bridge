c08223659d2c48856e88223a5d8f73ecee10a60c
BibartAlexandru <bibartalexandruixc@gmail.com>
2025-01-19 11:16:16 +0200

    fixed deploy contract

--- backend-js/index.js
+++ backend-js/index.js
@@ -5,10 +5,14 @@ const port = 3000;
 const { Web3 } = require("web3");
 const fs = require("fs");
 const { json } = require("stream/consumers");
+const { strict } = require("assert");
+const { error } = require("console");
 const { abi, bytecode } = JSON.parse(
   fs.readFileSync("../bin/EthereumToken/IBTToken.json")
 );
 require("dotenv").config();
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+let CONTRACT_ADDRESS = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -21,28 +25,54 @@ app.listen(port, () => {
 app.post("/deployContract", async (req, res) => {
   const { chainId, deployerPrivateKey } = req.body;
 
-  console.log(chainId + "\n" + deployerPrivateKey);
+  // console.log(chainId + "\n" + deployerPrivateKey);
 
-  const web3 = new Web3(
-    new Web3.providers.HttpProvider(
-      `http://${chainId}.infura.io/v3/${process.env.INFURA_API_KEY}`
-    )
-  );
+  try {
+    const web3 = new Web3("http://127.0.0.1:8545/");
 
-  const signer = web3.eth.accounts.privateKeyToAccount(deployerPrivateKey);
-  web3.eth.accounts.wallet.add(signer);
+    const signer = web3.eth.accounts.privateKeyToAccount(deployerPrivateKey);
+    web3.eth.accounts.wallet.add(signer);
 
-  const contract = new web3.eth.Contract(abi);
-  contract.options.data = bytecode;
-  const deployTx = contract.deploy();
-  const deployedContract = await deployTx
-    .send({
-      from: signer.address,
-      gas: await deployTx.estimateGas(),
-    })
-    .once("transactionHash", (txHash) => {
-      console.log(`Waiting for deployment of transaction ...`);
-      console.log(`https://${chainId}.etherscan.io/tx/${txhash}`);
+    const contract = new web3.eth.Contract(abi);
+    const deployTx = contract.deploy({
+      data: "0x" + bytecode,
     });
-  res.status(200).send();
+    const deployedContract = await deployTx
+      .send({
+        from: signer.address,
+        gas: 30000000,
+        // gas: await deployTx.estimateGas(),
+        // gasPrice: web3.utils.toWei("1", "gwei"),
+      })
+      .once("transactionHash", (txHash) => {
+        console.log(txHash);
+        //0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9
+      });
+    CONTRACT_ADDRESS = deployedContract.options.address;
+    res.status(200).send();
+  } catch (e) {
+    res.status(500).send({ error: e });
+  }
 });
+
+app.get("/balanceOf/:account", async (req, res) => {
+  const { account } = req.params;
+  if (!CONTRACT_ADDRESS) {
+    return res.status(401).send({ error: "Contract not deployed." });
+  }
+
+  try {
+    const web3 = new Web3("http://127.0.0.1:8545/");
+    const contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+    const balance = (
+      await contract.methods.balanceOf(account).call()
+    ).toString();
+    // console.log(`Balance: ${balance}, type: ${typeof balance}`);
+    return res.status(200).send({ balance });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send({ error: e });
+  }
+});
+
+app.post("/mintToPerson/:account/:amount", async (req, res) => {});
