acb0d283f624f7705c7196409286800cf2bda9ea
BibartAlexandru <bibartalexandruixc@gmail.com>
2025-01-23 19:50:16 +0200

    finished eth endpoints

--- backend-js/routes/eth.js
+++ backend-js/routes/eth.js
@@ -1,20 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+import IBTToken from "../../bin/EthereumToken/IBTToken.json" with {type: "json"};
+import {Web3} from "web3"
 const router = express.Router();
+dotenv.config();
+
+const ETH_DEPLOYER_PRIVATE_KEY = process.env.ETH_DEPLOYER_PRIVATE_KEY;
+const ETH_CHAIN_URL = process.env.ETH_CHAIN_URL;
+let ETH_CONTRACT_ADDRESS = process.env.ETH_CONTRACT_ADDRESS; // can be set in deployContract if not set in env
 
 router.get("/", (req, res) => {
   res.status(200).send("Hello from eth middleware");
 });
 
-router.post("/deployContract", async (req, res) => {
+router.get("/deployContract", async (req, res) => {
   try {
     const web3 = new Web3(ETH_CHAIN_URL);
     const signer = web3.eth.accounts.privateKeyToAccount(
       ETH_DEPLOYER_PRIVATE_KEY
     );
     web3.eth.accounts.wallet.add(signer);
-    const contract = new web3.eth.Contract(abi);
+    const contract = new web3.eth.Contract(IBTToken.abi);
     const deployTx = contract.deploy({
-      data: "0x" + bytecode,
+      data: "0x" + IBTToken.bytecode,
     });
     const deployedContract = await deployTx.send({
       from: signer.address,
@@ -22,25 +30,71 @@ router.post("/deployContract", async (req, res) => {
       // gas: await deployTx.estimateGas(),
       // gasPrice: web3.utils.toWei("1", "gwei"),
     });
-    CONTRACT_ADDRESS = deployedContract.options.address;
-    res.status(200).send();
+    ETH_CONTRACT_ADDRESS = deployedContract.options.address;
+    res.status(200).send({ETH_CONTRACT_ADDRESS});
   } catch (e) {
+    console.error(e);
     res.status(500).send({ error: e });
   }
 });
 
-router.get("/balanceOf/:account", async (req, res) => {
-  const { account } = req.params;
-  if (!CONTRACT_ADDRESS) {
+router.get('/mint/:pubKey/:amount', async (req,res) => {
+  let {pubKey, amount} = req.params;
+  if (!ETH_CONTRACT_ADDRESS) {
     return res.status(401).send({ error: "Contract not deployed." });
   }
+  try{
+    const web3 = new Web3(ETH_CHAIN_URL);
+    const contract = new web3.eth.Contract(IBTToken.abi, ETH_CONTRACT_ADDRESS);
+    pubKey = pubKey.slice(2);
+    await contract.methods.mintToPerson(pubKey,amount).send({
+      from: web3.eth.accountProvider.privateKeyToAccount(ETH_DEPLOYER_PRIVATE_KEY).address,
+      gas: 30000000,
+    });
+    return res.status(200).send({ message: "ok"});
+  }
+  catch(e){
+    console.error(e);
+    return res.status(500).send({error: e})
+  }
+});
 
+router.get('/burn/:pubKey/:amount', async (req,res) => {
+  let {pubKey, amount} = req.params;
+  if (!ETH_CONTRACT_ADDRESS) {
+    return res.status(401).send({ error: "Contract not deployed." });
+  }
+  try{
+    const web3 = new Web3(ETH_CHAIN_URL);
+    const contract = new web3.eth.Contract(IBTToken.abi, ETH_CONTRACT_ADDRESS);
+    pubKey = pubKey.slice(2);
+    await contract.methods.burnFromPerson(pubKey,amount).send({
+      from: web3.eth.accountProvider.privateKeyToAccount(ETH_DEPLOYER_PRIVATE_KEY).address,
+      gas: 30000000,
+    });
+    return res.status(200).send({ message: "ok"});
+  }
+  catch(e){
+    console.error(e);
+    return res.status(500).send({error: e})
+  }
+});
+
+router.get("/balanceOf/:pubKey", async (req, res) => {
+  let { pubKey } = req.params;
+  if (!ETH_CONTRACT_ADDRESS) {
+    return res.status(401).send({ error: "Contract not deployed." });
+  }
   try {
     const web3 = new Web3(ETH_CHAIN_URL);
-    const contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
-    const balance = (
-      await contract.methods.balanceOf(account).call()
-    ).toString();
+    const contract = new web3.eth.Contract(IBTToken.abi, ETH_CONTRACT_ADDRESS);
+    // are 0x in fata si trebe scos
+    pubKey = pubKey.slice(2);
+    let balance = await contract.methods.balanceOf(pubKey).call({
+        gas: 300000000,
+        from: web3.eth.accountProvider.privateKeyToAccount(ETH_DEPLOYER_PRIVATE_KEY).address
+      });
+    balance = Number(balance);
     return res.status(200).send({ balance });
   } catch (e) {
     console.error(e);
--- backend-js/routes/sui.js
+++ backend-js/routes/sui.js
@@ -15,8 +15,8 @@ const __dirname = import.meta.dirname;
 dotenv.config();
 const SUI_DEPLOYER_PRIVATE_KEY = process.env.SUI_DEPLOYER_PRIVATE_KEY;
 const SUI_CHAIN_URL = process.env.SUI_CHAIN_URL;
-let SUI_PACKAGE_OBJ_ID =
-  "0xfecdf7cd7fd1c02524eb224d3b411eec06e4544f6cf26048b51e9f73af22768a"; // set in the deploy contract endpoint
+let SUI_PACKAGE_ID = process.env.SUI_PACKAGE_ID; // set in the deploy contract endpoint if not set in env var
+
 function getDeployerKeypair() {
   const { words } = bech32.decode(SUI_DEPLOYER_PRIVATE_KEY);
   const numArray = bech32.fromWords(words);
@@ -83,10 +83,9 @@ router.get("/deployContract", async (req, res) => {
     },
   });
 
-  SUI_PACKAGE_OBJ_ID = txBlock.effects.created.find(
-    (o) => o.owner === "Immutable"
-  ).reference.objectId;
-  console.log(`SUI_PKG_ID: ${SUI_PACKAGE_OBJ_ID}`);
+  SUI_PACKAGE_ID = txBlock.effects.created.find((o) => o.owner === "Immutable")
+    .reference.objectId;
+  console.log(`SUI_PKG_ID: ${SUI_PACKAGE_ID}`);
   res.status(200).send({ txBlock });
 });
 
@@ -100,7 +99,7 @@ router.get("/mint/:recipientAddr/:amount", async (req, res) => {
     let cap = await client.getOwnedObjects({
       owner: keypair.toSuiAddress(),
       filter: {
-        StructType: `0x2::coin::TreasuryCap<${SUI_PACKAGE_OBJ_ID}::ibt_token::IBT_TOKEN>`,
+        StructType: `0x2::coin::TreasuryCap<${SUI_PACKAGE_ID}::ibt_token::IBT_TOKEN>`,
       },
       limit: 1,
     });
@@ -112,7 +111,7 @@ router.get("/mint/:recipientAddr/:amount", async (req, res) => {
     console.log(cap);
     const tx = new Transaction();
     tx.moveCall({
-      target: `${SUI_PACKAGE_OBJ_ID}::ibt_token::mint`,
+      target: `${SUI_PACKAGE_ID}::ibt_token::mint`,
       arguments: [
         tx.pure("u64", Number(amount)),
         tx.object(cap.data.objectId),
@@ -132,6 +131,9 @@ router.get("/mint/:recipientAddr/:amount", async (req, res) => {
 });
 
 router.get("/balance/:suiAddr", async (req, res) => {
+  if (SUI_PACKAGE_ID === undefined) {
+    return res.status(401).send({ error: "Contract not deployed." });
+  }
   const keypair = getDeployerKeypair();
   const { suiAddr } = req.params;
   const client = new SuiClient({
@@ -149,7 +151,7 @@ router.get("/balance/:suiAddr", async (req, res) => {
       //   showDisplay: true,
       // },
       filter: {
-        StructType: `0x2::coin::Coin<${SUI_PACKAGE_OBJ_ID}::ibt_token::IBT_TOKEN>`,
+        StructType: `0x2::coin::Coin<${SUI_PACKAGE_ID}::ibt_token::IBT_TOKEN>`,
       },
     });
 
@@ -160,7 +162,7 @@ router.get("/balance/:suiAddr", async (req, res) => {
     const coinIds = coins.data.map((c) => c.data.objectId);
 
     tx.moveCall({
-      target: `${SUI_PACKAGE_OBJ_ID}::ibt_token::balance`,
+      target: `${SUI_PACKAGE_ID}::ibt_token::balance`,
       arguments: [
         tx.makeMoveVec({
           objects: coinIds.map((cId) => tx.object(cId)),
